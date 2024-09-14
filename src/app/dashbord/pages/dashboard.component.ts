bc4ff759fe5b65d2589e9475b5f639c9fcbfae8d
Anner Escobar <70228927+AnnerEscobar@users.noreply.github.com>
2024-09-14 14:12:53 -0600

    adding searchBar, fixing dashboard

--- src/app/dashbord/pages/dashboard.component.ts
+++ src/app/dashbord/pages/dashboard.component.ts
@@ -1,21 +1,27 @@
 import { DashServiceService } from './../services/dash-service.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/services/login-service.service';
-import { userResponse } from '../interfaces/user-dash-response';
 import { ToastrService } from 'ngx-toastr';
-import { error } from 'console';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { userResponse } from '../interfaces/user-dash-response';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
 
   users: any[] = [];
+  p: number = 1; // Página actual
+  activeUsersCount: number = 0;
+  suspendedUsersCount: number = 0;
+  searchTerm:string ='';
+  filteredUsers: any[] = [];
 
   constructor(
     private authService: AuthService,
@@ -26,7 +32,7 @@ export class DashboardComponent implements OnInit {
 
 
   ngOnInit(): void {
-this.getUsers();
+    this.getUsers();
   }
 
 
@@ -34,21 +40,42 @@ this.getUsers();
     this.authService.closeSesion();
   }
 
-  getUsers():void{
+  getUsers(): void {
     this.dashService.getUsers()
-      .subscribe( data => {
-        console.log({data});
+      .subscribe(data => {
+        console.log({ data });
         this.users = data;
+        this.filteredUsers = data;
+        this.countUserStatus();
       },
-        (error) =>{
+        (error) => {
           this.toast.error('Error al obtener la data', 'Error', error)
           console.log(error)
         }
       )
   }
 
+  countUserStatus(): void {
+    this.activeUsersCount = this.users.filter(user => user.status === true).length;
+    this.suspendedUsersCount = this.users.filter(user => user.status === false).length;
+  }
 
+  normalizeString(str: string): string {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
 
+  filterUsers(): void {
+    const normalizedSearchTerm = this.normalizeString(this.searchTerm);
+
+    if (normalizedSearchTerm.trim() === '') {
+      this.filteredUsers = this.users;
+    } else {
+      this.filteredUsers = this.users.filter(user =>
+        this.normalizeString(user.name).toLowerCase().includes(normalizedSearchTerm.toLowerCase())
+      );
+    }
+    this.countUserStatus();
+  }
 
 
 }
